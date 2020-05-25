fdbc041bcda9a80d3977ea06ccc114381db36356
Anubhav Srivastava <anu.ritz80@gmail.com>
2020-05-25 20:51:33 +0530

    Showing Child Component on Parent Component.

---
+++ lwc/accountTile/accountTile.js
@@ -0,0 +1,19 @@
+import { LightningElement, api } from 'lwc';
+
+export default class AccountTile extends LightningElement {
+ @api account;
+ 
+
+    handleSelect(event){
+        
+        event.preventDefault();
+        const selectEvent = new CustomEvent(
+            'select',
+            {
+                detail : this.account.Id
+            }
+            
+        );
+        this.dispatchEvent(selectEvent);
+    }
+}
